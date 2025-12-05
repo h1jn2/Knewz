864e86330b31036c060cd1f746253031a44f9d45
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-12-05 18:19:10 +0900

    [Fix] 언론사 추가

--- app/src/main/java/com/example/knewz/util/MediaMapping.kt
+++ app/src/main/java/com/example/knewz/util/MediaMapping.kt
@@ -1,8 +1,7 @@
 package com.example.knewz.util
-
 val MEDIA_MAPPING: Map<String, String> = mapOf(
     // ----------------------------------------------------
-    // 1. 주요 일간지 및 종합 언론
+    // 1. 주요 일간지 / 종합지
     // ----------------------------------------------------
     "chosun.com" to "조선일보",
     "joongang.co.kr" to "중앙일보",
@@ -12,43 +11,42 @@ val MEDIA_MAPPING: Map<String, String> = mapOf(
     "khan.co.kr" to "경향신문",
     "seoul.co.kr" to "서울신문",
     "munhwa.com" to "문화일보",
-    "kyeongin.com" to "경인일보",
-    "kookmin.net" to "국민일보",
-    "sisaweek.com" to "시사위크",
-    "dailian.co.kr" to "데일리안",
-    "newdaily.co.kr" to "뉴데일리",
-    "nocutnews.co.kr" to "노컷뉴스",
-    "sisain.co.kr" to "시사IN",
-    "pressian.com" to "프레시안",
-    "ohmynews.com" to "오마이뉴스",
-    "mediatoday.co.kr" to "미디어오늘",
+    "segye.com" to "세계일보",
+    "kookje.co.kr" to "국제신문",
+    "busan.com" to "부산일보",
+    "gukjenews.com" to "국제뉴스",
+    "jejunews.com" to "제주일보",
     "jejudomin.co.kr" to "제주의소리",
 
     // ----------------------------------------------------
-    // 2. 경제/비즈니스 전문지
+    // 2. 경제 / 비즈니스 / IT 전문지
     // ----------------------------------------------------
     "mk.co.kr" to "매일경제",
     "hankyung.com" to "한국경제",
-    "moneytoday.co.kr" to "머니투데이",
+    "sedaily.com" to "서울경제",
     "edaily.co.kr" to "이데일리",
     "fnnews.com" to "파이낸셜뉴스",
     "etoday.co.kr" to "이투데이",
-    "sedaily.com" to "서울경제",
-    "mtn.co.kr" to "머니투데이방송(MTN)",
-    "bizwatch.co.kr" to "비즈워치",
+    "moneytoday.co.kr" to "머니투데이",
+    "biz.chosun.com" to "조선비즈",
+    "biz.khan.co.kr" to "경향비즈",
     "etnews.com" to "전자신문",
+    "ddaily.co.kr" to "디지털데일리",
     "bloter.net" to "블로터",
     "zdnet.co.kr" to "ZDNet Korea",
-    "ddaily.co.kr" to "디지털데일리",
     "inews24.com" to "아이뉴스24",
     "thebell.co.kr" to "더벨",
-    "paxnetnews.com" to "팍스경제TV",
-    "segye.com" to "세계일보",
+    "paxnetnews.com" to "팍스넷뉴스",
+    "newsworks.co.kr" to "뉴스워커",
+    "kinews.net" to "기자숲",
+    "hellodd.com" to "헬로디디",
+    "dailysecu.com" to "데일리시큐",
 
     // ----------------------------------------------------
-    // 3. 방송사 및 통신사
+    // 3. 방송사 / 통신사
     // ----------------------------------------------------
     "yonhapnews.co.kr" to "연합뉴스",
+    "newsis.com" to "뉴시스",
     "ytn.co.kr" to "YTN",
     "kbs.co.kr" to "KBS",
     "mbc.co.kr" to "MBC",
@@ -57,49 +55,62 @@ val MEDIA_MAPPING: Map<String, String> = mapOf(
     "tvchosun.com" to "TV조선",
     "mbn.co.kr" to "MBN",
     "ichannela.com" to "채널A",
-    "obsnews.co.kr" to "OBS",
-    "newsis.com" to "뉴시스",
-    "asiatoday.co.kr" to "아시아투데이",
+    "obs.co.kr" to "OBS",
+    "ebs.co.kr" to "EBS",
 
     // ----------------------------------------------------
-    // 4. 스포츠/연예 전문
+    // 4. 연예 / 스포츠 전문
     // ----------------------------------------------------
     "sportschosun.com" to "스포츠조선",
-    "isplus.com" to "일간스포츠",
+    "sportsseoul.com" to "스포츠서울",
+    "joins.com" to "일간스포츠",
     "starnews.com" to "스타뉴스",
-    "spotvnews.com" to "스포티비뉴스",
     "osen.co.kr" to "OSEN",
     "newsen.com" to "뉴스엔",
     "xportsnews.com" to "엑스포츠뉴스",
-    "sports.hankooki.com" to "스포츠한국",
+    "spotvnews.com" to "스포티비뉴스",
     "mydaily.co.kr" to "마이데일리",
+    "stoo.com" to "스투닷컴",
 
     // ----------------------------------------------------
-    // 5. 기타 전문/지역 언론
+    // 5. 지역 / 전문 / 기타 언론
     // ----------------------------------------------------
-    "doctorsnews.co.kr" to "의사신문",
+    "ajunews.com" to "아주경제",
     "lawtimes.net" to "법률신문",
     "kukinews.com" to "쿠키뉴스",
-    "healthnews.co.kr" to "헬스조선",
-    "hankyung.com" to "한국경제TV",
-    "cbs.co.kr" to "CBS 노컷뉴스",
-    "ajunews.com" to "아주경제",
-    "seoulfn.com" to "서울파이낸스",
-    "energy-news.co.kr" to "에너지경제",
     "dailymedi.com" to "데일리메디",
-    "ikangwon.com" to "강원일보",
-    "imaeil.com" to "매일신문(대구)",
-    "busan.com" to "부산일보",
-    "kwangju.co.kr" to "광주매일신문",
-    "jeonmin.co.kr" to "전북도민일보",
-    "cynews.co.kr" to "충청일보",
+    "doctorsnews.co.kr" to "의사신문",
+    "health.chosun.com" to "헬스조선",
+    "kookmin.co.kr" to "국민일보",
+    "cbs.co.kr" to "CBS 노컷뉴스",
+    "kmib.co.kr" to "국민일보",
+    "news1.kr" to "뉴스1",
+    "pressian.com" to "프레시안",
+    "ohmynews.com" to "오마이뉴스",
+    "sisain.co.kr" to "시사IN",
+    "mediatoday.co.kr" to "미디어오늘",
+    "sisapress.com" to "시사저널",
+    "ilyo.co.kr" to "일요신문",
+    "weekly.chosun.com" to "주간조선",
+    "weekly.hankooki.com" to "주간한국",
+    "womennews.co.kr" to "여성신문",
+    "christiantoday.co.kr" to "크리스천투데이",
+    "kmib.co.kr" to "국민일보",
+    "breaknews.com" to "브레이크뉴스",
+
+    // 지역(광역)
+    "knnews.co.kr" to "경남신문",
+    "ccdailynews.com" to "충청일보",
     "dailycc.net" to "충청신문",
-    "dtnews24.com" to "디트뉴스24",
-    "incheontoday.com" to "인천투데이",
-    "gukjenews.com" to "국제뉴스",
+    "jeonmin.co.kr" to "전민일보",
+    "jeonbukilbo.com" to "전북일보",
+    "imaeil.com" to "매일신문",
+    "idomin.com" to "경남도민일보",
+    "incheonilbo.com" to "인천일보",
+    "ikjangwon.com" to "강원일보",
 
     // ----------------------------------------------------
-    // 6. 포털 및 플랫폼 (도메인 전체 일치 필요)
+    // 6. 포털
     // ----------------------------------------------------
     "naver.com" to "네이버 뉴스",
     "daum.net" to "다음 뉴스"
