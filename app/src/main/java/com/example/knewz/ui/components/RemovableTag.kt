924f19e53e1d7bc94c287b897a43d21fa1cec06b
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-12-19 14:59:06 +0900

    [UI] 최근 검색어 Tag Component

---
+++ app/src/main/java/com/example/knewz/ui/components/RemovableTag.kt
@@ -0,0 +1,55 @@
+package com.example.knewz.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.knewz.ui.theme.StrokeGray
+
+@Composable
+fun RemovableTag(tagName: String) {
+    Row(
+        modifier = Modifier
+            .sizeIn(minWidth = 50.dp, minHeight = 24.dp)
+            .background(Color.White, RoundedCornerShape(10.dp))
+            .border(
+                BorderStroke(1.dp, StrokeGray),
+                RoundedCornerShape(10.dp)
+            )
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Clear,
+            contentDescription = "Clear",
+            modifier = Modifier
+                .size(18.dp)
+                .clickable {}
+        )
+        Spacer(Modifier.width(4.dp))
+        Text(
+            text = tagName,
+            style = MaterialTheme.typography.labelLarge,
+            color = Color.Black
+        )
+    }
+}
---
+++ app/src/main/java/com/example/knewz/ui/components/RemovableTagsRow.kt
@@ -0,0 +1,25 @@
+package com.example.knewz.ui.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RemovableTagsRow(tags: List<String>) {
+    val scrollState = rememberScrollState()
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .horizontalScroll(scrollState),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        tags.forEach { tag ->
+            RemovableTag(tagName = tag)
+        }
+    }
+}
