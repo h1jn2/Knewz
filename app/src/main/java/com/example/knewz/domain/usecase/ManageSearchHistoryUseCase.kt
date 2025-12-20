56bd770df3dea316d98ca43afa4c5ce7f8e25294
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-12-20 16:07:33 +0900

    [Refactor] 최근 검색어 저장 로직 분리

---
+++ app/src/main/java/com/example/knewz/domain/usecase/ManageSearchHistoryUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.knewz.domain.usecase
+
+import javax.inject.Inject
+
+class ManageSearchHistoryUseCase @Inject constructor(
+) {
+    suspend fun saveQuery(query: String) {
+
+    }
+}
