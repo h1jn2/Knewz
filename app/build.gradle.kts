59f2ea14d4126ddf1d1f3b05c0f15770916c1e52
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2026-01-12 15:40:41 +0900

    [Chore] RealTimeDatabase Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.firebase.database)
     ksp(libs.hilt.compiler)
 
 // Room
